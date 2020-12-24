2ed2cc53607368ce4bad4151752224a1071e69db
medbm5 <medbaamrani0@gmail.com>
2020-12-24 18:04:01 +0100

    ikhan

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ app.post("/compose_show", function(req, res){
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.port, function() {
   console.log("Server started on port 3000");
 });
 
